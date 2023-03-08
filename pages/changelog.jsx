b192d4abbd62213a50a2d2ce2ddbd7db6991f9a2
Medea <logmedha09@gmail.com>
2023-03-08 18:00:20 +0000

    added markdown support to changelog

--- pages/changelog.jsx
+++ pages/changelog.jsx
@@ -1,7 +1,15 @@
 import Layout from '../components/layout'
 import styles from '../styles/changelog.module.css';
 
-const changelog = {"2023-03-07": "Added a changelog"}
+const changelog_dict = {"2023-03-07": "- Added a changelog"}
+var changelog = {}
+for (const [key, value] of Object.entries(changelog_dict)) {
+  var showdown  = require('showdown'),
+    converter = new showdown.Converter(),
+    text      = value,
+    html      = converter.makeHtml(text);
+  changelog[key] = html
+}
 
 export default function ChangeLog() {
   return (
@@ -9,9 +17,11 @@ export default function ChangeLog() {
       <h1>Changelog</h1>
       {
         Object.keys(changelog).map((date, index) => (
+          
           <div id={date} className={styles.day}>
-            <p class="light">{date}</p>
-            <p>{changelog[date]}</p>
+            <p className="light">{date}</p>
+{/*             <p>{changelog[date]}</p> */}
+            <div dangerouslySetInnerHTML={{ __html: changelog[date] }} />
             <br/>
           </div>
         ))
@@ -19,3 +29,4 @@ export default function ChangeLog() {
     </Layout>
   );
 }
+
