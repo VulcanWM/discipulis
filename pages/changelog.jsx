f978ba02b7a4a76a513599091730467d88c292e9
Medea <logmedha09@gmail.com>
2023-03-08 21:52:42 +0000

    changed changelog features

--- pages/changelog.jsx
+++ pages/changelog.jsx
@@ -1,7 +1,7 @@
 import Layout from '../components/layout'
 import styles from '../styles/changelog.module.css';
 
-const changelog_dict = {"2023-03-07": "- Added a changelog"}
+const changelog_dict = {"2023-03-08": "- Added a changelog", "2023-03-05": "- Added the `convert_verb` and `convert_noun` pages", "2023-03-04": "- Added the ability to browse sets\n- Added set pages to see each set's vocab", "2023-03-03": "- Added table pages for both nouns and verbs with all tenses and cases"}
 var changelog = {}
 for (const [key, value] of Object.entries(changelog_dict)) {
   var showdown  = require('showdown'),
@@ -20,7 +20,6 @@ export default function ChangeLog() {
           
           <div id={date} className={styles.day}>
             <p className="light">{date}</p>
-{/*             <p>{changelog[date]}</p> */}
             <div dangerouslySetInnerHTML={{ __html: changelog[date] }} />
             <br/>
           </div>
@@ -29,4 +28,3 @@ export default function ChangeLog() {
     </Layout>
   );
 }
-
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,16 +1,21 @@
 import Layout from '../components/layout'
+import Link from 'next/link';
 
 export default function HomePage() {
   return (
     <Layout pageTitle="Home">
       <h1>Discipulis</h1>
-      <p>Welcome to Discipulis.</p>
-      <p>Here you can be tested on latin nouns and verbs.</p>
-      <p>You can also use the converters to convert nouns and verbs from english to latin</p>
-      <a href="/all_nouns">All Nouns</a><br/>
-      <a href="/all_verbs">All Verbs</a><br/>
-      <a href="/convert_verb">Convert verb from english to latin</a><br/>
-      <a href="/convert_noun">Convert noun from english to latin</a><br/>
+      <p>Discipulis is a website whose ultimate aim is to help children around the world learn Latin in a fun way.</p>
+      <h2>Its main features:</h2>
+      <ul>
+        <li>Shows all the tenses of a Latin verb in the form of a table</li>
+        <li>Shows all the cases of a Latin noun in the form of a table</li>
+        <li>Use test mode on the table pages to quiz yourself on cases and tenses</li>
+        <li>Flip the cells on the tables so you can use them as flashcards</li>
+        <li>Has converters to convert English nouns and verbs to Latin</li>
+        <li>Easily quiz yourself on vocab with the different sets</li>
+      </ul>
+      <Link href="/changelog">View the full changelog</Link>
     </Layout>
   );
 }
