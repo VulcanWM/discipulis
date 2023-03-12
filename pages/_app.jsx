8267ce0123b2894610b0240959dddeb2e2d2c50d
VulcanWM <logmedha09@gmail.com>
2023-03-12 20:43:56 +0000

    made default theme light

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -3,7 +3,7 @@ import { ThemeProvider } from 'next-themes';
 
 export default function App({ Component, pageProps }) {
   return (
-		<ThemeProvider>
+		<ThemeProvider defaultTheme = 'light'>
 			<Component {...pageProps} />
 		</ThemeProvider>
 	);
