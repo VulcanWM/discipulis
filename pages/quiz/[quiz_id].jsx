8f579b8ab95b2931937f3de6334af7ebcd8b3e6c
VulcanWM <logmedha09@gmail.com>
2023-03-31 20:27:15 +0100

    added state for count&score for automatic updates

--- pages/quiz/[quiz_id].jsx
+++ pages/quiz/[quiz_id].jsx
@@ -1,25 +1,63 @@
 import Layout from "../../components/layout";
 import {generate_question} from '../../lib/generate_question'
+import { useState } from 'react';
+import Link from "next/link";
+import { useForm } from "react-hook-form";
 
 export default function Question( {question_lists, quiz_id, answer_type, question_type} ) {
-  var count = 0
+  const [end_quiz, setShowMe] = useState(false);
+  const [count, setCount] = useState(0);
+  const [score, setScore] = useState(0);
+  function toggleEnd(){
+    setShowMe(!end_quiz);
+  }
   function new_question(){
     if (count == 9){
-        document.getElementById("form").innerText = "Quiz ended!"
+      toggleEnd()
     } else {
-        count++
-        let question_dict = question_lists[count]
-        document.getElementById("question").innerText = question_dict.question
-        document.getElementById("question_number").innerText = `Question: ${count + 1}`
+      setCount(count + 1)
     }
   }
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm();
+  const onSubmit = (data) => {
+    if (question_lists[count].answers.split(",").includes(data.guess.toLowerCase()) == true){
+        setScore(score + 1)
+    } 
+    new_question()
+  };
   return (
     <Layout pageTitle={quiz_id} wordtype="quiz">
-      <h1>Question</h1>
-      <div id="form">
-        <p id="question_number">Question: {count + 1}</p>
-        <p id="question">{question_lists[0].question}</p>
-        <button onClick={new_question}>New Question</button>
+      <h1>{quiz_id} Quiz</h1>
+      <div style={{
+        display: end_quiz?"none":"block"
+      }}>
+        <p>Question <strong>{count + 1}</strong> out of 10</p>
+        <p>Score: <strong>{score}</strong></p>
+        <hr/>
+        <p id="question">{question_lists[count].question}</p>
+        {question_lists[count].choices == "input" ? (
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <input {...register("guess", { required: true })} placeholder="answer" type="text"/><br/>
+            <input type="submit" value="submit answer"/>
+          </form>
+        ) : (
+            <>
+              <p>choices</p>
+              <button onClick={new_question}>New Question</button>
+            </>
+        )}
+      </div>
+      <div id="endquiz" style={{
+        display: end_quiz?"block":"none"
+      }}>
+        <p>You finished the quiz with the score of <strong>{score}</strong> out of 10!</p>
+        <Link href={"/quiz/" + quiz_id + "?answer_type=" + answer_type + "&question_type=" + question_type}>same quiz - same question types</Link>
+        <br/>
+        <Link href={"/start_quiz/" + quiz_id}>same quiz - different type questions</Link>
       </div>
     </Layout>
   );
